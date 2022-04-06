d99a0fd65d5a3f9223daa976945ada05f120e51c
Rasul Zeyalov <rasul.zeynalov.95@gmail.com>
2022-04-07 03:31:45 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from './pages/Home';
+import Product from './pages/Product';
+
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Product/>
     </div>
   );
 }
