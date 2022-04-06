455a4c4437c2566ef730956bb810f4554f93f8c8
Rasul <rasul.zeynalov.95@gmail.com>
2022-04-06 19:51:58 +0400

    Fayl yuklendi

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: grey;
+    color: white;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight:500;
+`;
+
+const Announcement = () => {
+  return (
+    <Container>
+        Announcement
+    </Container>
+  )
+}
+
+export default Announcement
